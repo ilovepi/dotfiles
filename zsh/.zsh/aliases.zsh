5396a4b4e67031da75facba9dd18c37bfa86655c
Paul Kirth <pk1574@gmail.com>
2018-05-10 19:20:17 -0700

    Add new aliases to zsh

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,5 +1,15 @@
 
-alias zshconfig="edit ~/.zshrc"
+#Editor aliases
+alias zshconfig="edit /dotfiles/zsh/.zshrc"
+alias zprofile="edit ~/dotfiles/zsh/.zprofile"
+alias zshplug="edit ~/dotfiles/zsh/.zsh/zplug.zsh"
+alias ztheme="edit ~/dotfiles/zsh/.zsh/theme.zsh"
+alias zaliases="edit ~/dotfiles/zsh/.zsh/aliases.zsh"
+alias ztheme="edit ~/dotfiles/zsh/.zsh/theme.zsh"
+alias ztheme="edit ~/dotfiles/zsh/.zsh/theme.zsh"
+alias nvimrc="edit ~/dotfiles/config/nvim/init.vim"
+alias vv="edit $(fd | fzf)"
+
 alias :q='exit'
 alias lh='ls -ld .?*'
 alias magit='nvim -c MagitOnly'
