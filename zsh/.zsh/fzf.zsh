878a7f94897080f4b194c3ac5ec62b1b811c29e1
Paul Kirth <pk1574@gmail.com>
2018-05-21 07:12:52 +0000

    add clangd to llvm script

--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -35,3 +35,28 @@ fed() {
   fi
 }
 
+
+clang-format-all() {
+  FMT=clang-format
+  # Check all of the arguments first to make sure they're all directories
+  for dir in "$@"; do
+    if [ ! -d "${dir}" ]; then
+        echo "${dir} is not a directory"
+        usage
+    fi
+  done
+
+  # Run clang-format -i on all of the things
+  for dir in "$@"; do
+    pushd "${dir}" &>/dev/null
+    find . \
+         \( -name '*.c' \
+         -o -name '*.cc' \
+         -o -name '*.cpp' \
+         -o -name '*.h' \
+         -o -name '*.hh' \
+         -o -name '*.hpp' \) \
+         -exec "${FMT}" -i '{}' \;
+    popd &>/dev/null
+  done
+}
