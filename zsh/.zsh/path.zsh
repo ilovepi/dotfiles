6afa89e63c2ee9f85de30b1af9938ff9c49b594d
Paul Kirth <pk1574@gmail.com>
2019-06-27 14:31:34 -0700

    Add terminator themes and layouts

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -11,28 +11,12 @@ PATH=$HOME/.local/bin:$PATH
 PATH=$PATH:/usr/lib/llvm-7/share
 
 # add Go directories to PATH
-#export GOROOT=/usr/local/go
 export GOPATH=$HOME/workspace/go
 
-#PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 PATH=$GOPATH/bin:$PATH
 
 #add cargo directories to PATH
 PATH=$HOME/rust-install/bin:$PATH
 PATH=$HOME/.cargo/bin:$PATH
-
-
-#export LLVM_SRC=$HOME/workspace/llvm-dev/llvm
-#export LLVM_OBJ=$HOME/workspace/llvm-dev/build
-#export LLVM_DIR=$HOME/workspace/llvm-dev/build
-
-export LLVM_SRC=$HOME/workspace/mpk/rust/src/llvm
-export LLVM_OBJ=$HOME/workspace/mpk/rust/build/x86_64-unknown-linux-gnu/llvm
-export LLVM_DIR=$HOME/workspace/mpk/rust/build/x86_64-unknown-linux-gnu/llvm
-
-export SVF_HOME=$HOME/workspace/svf
-
-PATH=$LLVM_DIR/bin:$PATH
-PATH=$SVF_HOME/Release-build/bin:$PATH
 
 export PATH
