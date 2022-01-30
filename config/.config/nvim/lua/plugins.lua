dc149cbd5698f464c745d724addd2d81e0f3744d
Paul Kirth <paulkirth@google.com>
2022-01-30 12:58:05 -0800

    [nvim] Fix bad merge

--- config/.config/nvim/lua/plugins.lua
+++ config/.config/nvim/lua/plugins.lua
@@ -6,33 +6,17 @@ return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     -- Completion
     use 'Chiel92/vim-autoformat'
-    --use 'Shougo/denite.nvim'
-    --use 'Shougo/deoplete.nvim'
-    --use 'Shougo/deorise.nvim'
-    --use 'SirVer/ultisnips'
-    --use 'autozimu/LanguageClient-neovim', {'rev' : 'next', 'build' : 'bash install.sh'}
     use 'honza/vim-snippets'
     use 'norcalli/snippets.nvim'
-    --use 'w0rp/ale'
     use 'dense-analysis/ale'
 
 
     -- Search & Navigation
     use 'brooth/far.vim'
-    --use {'junegunn/fzf', { 'build': './install --all', 'merged': 0 })}
     use {'junegunn/fzf', run = function() vim.fn['fzf#install']() end}
     use 'junegunn/fzf.vim'
     use {'raghur/fruzzy', run = function() vim.fn['fruzzy#install']() end}
     use 'kyazdani42/nvim-tree.lua'
-    --use { 
-        --'scrooloose/nerdtree',
-        --opt = true,
-        ----cmd = {'NERDTreeToggle'},
-        --keys = {'<Plug>(NERDTreeToggle)'},
-        --requires = {
-            --{'tiagofumo/vim-nerdtree-syntax-highlight', opt = true},
-        --}
-    --}
     use 'wincent/loupe'
 
     -- Appearance
@@ -50,8 +34,6 @@ return require('packer').startup(function()
     --use 'ryanoasis/vim-devicons'
     use 'kyazdani42/nvim-web-devicons'
     use 'trevordmiller/nova-vim'
-    --use 'vim-airline/vim-airline'
-    --use 'vim-airline/vim-airline-themes'
 
     -- Writing
     use 'junegunn/goyo.vim'
@@ -78,24 +60,17 @@ return require('packer').startup(function()
     --use 'greyblake/vim-preview'
 
     -- Vim navigation & editing essentials
-    --use 'bling/vim-bufferline'
     use 'chrisbra/NrrwRgn'
     use 'christoomey/vim-tmux-navigator'
     use 'farmergreg/vim-lastplace'
-    --use 'gcmt/wildfire.vim'
     use 'godlygeek/tabular'
-    --use 'jiangmiao/auto-pairs'
     use 'windwp/nvim-autopairs'
-    --use 'kana/vim-textobj-indent'
-    --use 'kana/vim-textobj-user'
-    --use 'mattn/webapi-vim'
     use 'mbbill/undotree'
     use 'nathanaelkane/vim-indent-guides'
     use 'osyo-manga/vim-over'
     use 'reedes/vim-litecorrect'
     use 'reedes/vim-textobj-quote'
     use 'reedes/vim-textobj-sentence'
-    --use 'rhysd/conflict-marker.vim'
     use 'preservim/nerdcommenter'
     use 'tpope/vim-repeat'
     use 'tpope/vim-sleuth'
@@ -113,7 +88,6 @@ return require('packer').startup(function()
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use 'nvim-lua/completion-nvim'
     use 'nvim-lua/lsp_extensions.nvim'
-    use 'kabouzeid/nvim-lspinstall'
     use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
     use {"ellisonleao/glow.nvim"}
 
