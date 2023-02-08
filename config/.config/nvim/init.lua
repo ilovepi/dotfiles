3e000ff0b78ddde6a5f5206195ebe5d26ed3b19f
Paul Kirth <pk1574@gmail.com>
2023-02-07 22:42:22 -0800

    [nvim] Redo LSP configuration
    - Also format code

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,10 +1,10 @@
-
 local fn = vim.fn
 
 -- Auto install packer.nvim if not exists
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+          install_path })
 end
 
 --- options
@@ -34,30 +34,30 @@ init_dirs()
 
 --- Basic Settings
 
-local o = vim.o
-local opt = vim.opt
+local o            = vim.o
+local opt          = vim.opt
 
-o.background = 'dark'
-o.mouse = 'a'                 --- Automatically enable mouse usage
-o.mousehide = true            --- Hide the mouse cursor while typing
+o.background       = 'dark'
+o.mouse            = 'a' --- Automatically enable mouse usage
+o.mousehide        = true --- Hide the mouse cursor while typing
 
-o.clipboard = 'unnamed,unnamedplus'
+o.clipboard        = 'unnamed,unnamedplus'
 
 --- Always switch to the current file directory
 --- autocmd BufEnter * if bufname("") !~ "^\[A-Za-z0-9\]*://" | lcd %:p:h | endif
 
 -- vim.api.nvim_create_autocmd("BufEnter", {})
-o.autochdir = true
+o.autochdir        = true
 
-o.shortmess   = vim.o.shortmess .. 'filmnrxoOtT'  --- Abbrev. of messages (avoids 'hit enter')
-o.viewoptions = 'folds,options,cursor,unix,slash' --- Better Unix / Windows compatibility
-o.virtualedit = 'onemore'                         --- Allow for cursor beyond last character
-o.history     = 1000                              --- Store a ton of history (default is 20)
-o.spell       = true                              --- Spell checking on
-o.hidden      = true                              --- Allow buffer switching without saving
-opt.iskeyword = opt.iskeyword - { '.' }           --- '.' is an end of word designator
-opt.iskeyword = opt.iskeyword - { '#' }           --- '#' is an end of word designator
-opt.iskeyword = opt.iskeyword - { '-' }           --- '-' is an end of word designator
+o.shortmess        = vim.o.shortmess .. 'filmnrxoOtT' --- Abbrev. of messages (avoids 'hit enter')
+o.viewoptions      = 'folds,options,cursor,unix,slash' --- Better Unix / Windows compatibility
+o.virtualedit      = 'onemore' --- Allow for cursor beyond last character
+o.history          = 1000 --- Store a ton of history (default is 20)
+o.spell            = true --- Spell checking on
+o.hidden           = true --- Allow buffer switching without saving
+opt.iskeyword      = opt.iskeyword - { '.' } --- '.' is an end of word designator
+opt.iskeyword      = opt.iskeyword - { '#' } --- '#' is an end of word designator
+opt.iskeyword      = opt.iskeyword - { '-' } --- '-' is an end of word designator
 
 vim.opt.signcolumn = 'yes:1'
 
@@ -66,54 +66,55 @@ vim.opt.signcolumn = 'yes:1'
 --- au FileType gitcommit au! BufEnter COMMIT_EDITMSG call setpos('.', [0, 1, 1, 0])
 
 ---  Setting up the directories
-o.backup = true                  --- Backups are nice ...
-o.undofile = true                --- So is persistent undo ...
-o.undolevels=1000         --- Maximum number of changes that can be undone
-o.undoreload=10000        --- Maximum number lines to save for undo on a buffer reload
+o.backup           = true --- Backups are nice ...
+o.undofile         = true --- So is persistent undo ...
+o.undolevels       = 1000 --- Maximum number of changes that can be undone
+o.undoreload       = 10000 --- Maximum number lines to save for undo on a buffer reload
 
 
 --- UI
 
-opt.cursorline     = true
-opt.showmode       = false
-opt.ruler          = true
-opt.rulerformat    = "%30(%= \\:b%n%y%m%r%w\\ %l,%c%V\\ %P%)"
-opt.showcmd        = false
-opt.colorcolumn    = '80'                                --- Puts a visual marker at the desired line width
-opt.backspace      = 'indent,eol,start'                  --- Backspace for dummies
-opt.linespace      = 0                                   --- No extra spaces between rows
-opt.number         = true                                --- Line numbers on
-opt.relativenumber = true                                --- Line numbers on
-opt.showmatch      = true                                --- Show matching brackets/parenthesis
-opt.incsearch      = true                                --- Find as you type search
-opt.hlsearch       = true                                --- Highlight search terms
-opt.winminheight   = 0                                   --- Windows can be 0 line high
-opt.ignorecase     = true                                --- Case insensitive search
-opt.smartcase      = true                                --- Case sensitive when uc present
-opt.wildmenu       = true                                --- Show list instead of just completing
-opt.wildmode       = "list:longest,full"                 --- Command <Tab> completion, list matches, then longest common part, then all.
-opt.whichwrap      = "b,s,h,l,<,>,[,]"                   --- Backspace and cursor keys wrap too
-opt.scrolljump     = 5                                   --- Lines to scroll when cursor leaves screen
-opt.scrolloff      = 3                                   --- Minimum lines to keep above and below cursor
-opt.foldenable     = true                                --- Auto fold code
-opt.list           = true
-opt.listchars      = 'tab:›\\ ,trail:•,extends:#,nbsp:.' --- Highlight problematic whitespace
+opt.cursorline       = true
+opt.showmode         = false
+opt.ruler            = true
+opt.rulerformat      = "%30(%= \\:b%n%y%m%r%w\\ %l,%c%V\\ %P%)"
+opt.showcmd          = false
+opt.colorcolumn      = '80' --- Puts a visual marker at the desired line width
+opt.backspace        = 'indent,eol,start' --- Backspace for dummies
+opt.linespace        = 0 --- No extra spaces between rows
+opt.number           = true --- Line numbers on
+opt.relativenumber   = true --- Line numbers on
+opt.showmatch        = true --- Show matching brackets/parenthesis
+opt.incsearch        = true --- Find as you type search
+opt.hlsearch         = true --- Highlight search terms
+opt.winminheight     = 0 --- Windows can be 0 line high
+opt.ignorecase       = true --- Case insensitive search
+opt.smartcase        = true --- Case sensitive when uc present
+opt.wildmenu         = true --- Show list instead of just completing
+opt.wildmode         =
+"list:longest,full" --- Command <Tab> completion, list matches, then longest common part, then all.
+opt.whichwrap        = "b,s,h,l,<,>,[,]" --- Backspace and cursor keys wrap too
+opt.scrolljump       = 5 --- Lines to scroll when cursor leaves screen
+opt.scrolloff        = 3 --- Minimum lines to keep above and below cursor
+opt.foldenable       = true --- Auto fold code
+opt.list             = true
+opt.listchars        = 'tab:›\\ ,trail:•,extends:#,nbsp:.' --- Highlight problematic whitespace
 
 --- formatting
-opt.wrap        = false   --- Do not wrap long lines
-opt.autoindent  = true    --- Indent at the same level of the previous line
-opt.expandtab   = true    --- Tabs are spaces, not tabs
-opt.joinspaces  = false   --- Prevents inserting two spaces after punctuation on a join (J)
-opt.splitright  = true    --- Puts new vsplit windows to the right of the current
-opt.splitbelow  = true    --- Puts new split windows to the bottom of the current
-opt.shiftwidth  = 4       --- Use indents of 4 spaces
-opt.tabstop     = 4       --- An indentation every four columns
-opt.softtabstop = 4       --- Let backspace delete indent
-opt.pastetoggle = "<F12>" --- pastetoggle (sane indentation on pastes)
+opt.wrap             = false --- Do not wrap long lines
+opt.autoindent       = true --- Indent at the same level of the previous line
+opt.expandtab        = true --- Tabs are spaces, not tabs
+opt.joinspaces       = false --- Prevents inserting two spaces after punctuation on a join (J)
+opt.splitright       = true --- Puts new vsplit windows to the right of the current
+opt.splitbelow       = true --- Puts new split windows to the bottom of the current
+opt.shiftwidth       = 4 --- Use indents of 4 spaces
+opt.tabstop          = 4 --- An indentation every four columns
+opt.softtabstop      = 4 --- Let backspace delete indent
+opt.pastetoggle      = "<F12>" --- pastetoggle (sane indentation on pastes)
 
 --- Key Bindings
 
-vim.g.mapleader = ' '
+vim.g.mapleader      = ' '
 vim.g.maplocalleader = '_'
 
 --- Easier moving in tabs and windows
@@ -138,23 +139,23 @@ vim.keymap.set("c", "cwd", "lcd %:p:h")
 vim.keymap.set("c", "cd.", "lcd %:p:h")
 
 
-vim.keymap.set("n", "j", "gj", {noremap=true})
-vim.keymap.set("n", "k", "gk", {noremap=true})
+vim.keymap.set("n", "j", "gj", { noremap = true })
+vim.keymap.set("n", "k", "gk", { noremap = true })
 
 
 --- Visual shifting (does not exit Visual mode)
-vim.keymap.set("v", "<", "<gv", {noremap=true})
-vim.keymap.set("v", ">", ">gv", {noremap=true})
+vim.keymap.set("v", "<", "<gv", { noremap = true })
+vim.keymap.set("v", ">", ">gv", { noremap = true })
 
 --- Easier formatting
-vim.keymap.set("n", "<leader>q", "gwip", {noremap=true, silent=true})
+vim.keymap.set("n", "<leader>q", "gwip", { noremap = true, silent = true })
 
 --- Toggle fold at current position.
-vim.keymap.set("n", "<Tab>", "za", {noremap=true})
+vim.keymap.set("n", "<Tab>", "za", { noremap = true })
 
 --- Allow using the repeat operator with a visual selection (!)
 --- http://stackoverflow.com/a/8064607/127816
-vim.keymap.set("v", ".", ":normal .<CR>", {noremap=true})
+vim.keymap.set("v", ".", ":normal .<CR>", { noremap = true })
 
 --- For when you forget to sudo.. Really Write the file.
 --- unfortunately this doesn't work in neovim :(
@@ -163,7 +164,7 @@ vim.keymap.set("v", ".", ":normal .<CR>", {noremap=true})
 
 -- Some helpers to edit mode
 -- http://vimcasts.org/e/14
-vim.keymap.set("c", "%%", "<C-R>=fnameescape(expand('%:h')).'/'<CR>", {noremap=true})
+vim.keymap.set("c", "%%", "<C-R>=fnameescape(expand('%:h')).'/'<CR>", { noremap = true })
 vim.keymap.set("v", "ew", ":e %%")
 vim.keymap.set("v", "es", ":sp %%")
 vim.keymap.set("v", "ev", ":vs %%")
@@ -231,8 +232,8 @@ vim.g.gruvbox_material_better_performance = 1
 
 
 --- UndoTree
-vim.g.undotree_SetFocusWhenToggle=1
-vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>", {noremap=true})
+vim.g.undotree_SetFocusWhenToggle = 1
+vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>", { noremap = true })
 
 --- Indent guides
 vim.g.indent_guides_start_level = 2
@@ -241,26 +242,26 @@ vim.g.indent_guides_enable_on_vim_startup = 1
 
 -- NvimTree
 
-vim.keymap.set("n", "<C-e>", ":NvimTreeToggle<CR>", {noremap=true})
+vim.keymap.set("n", "<C-e>", ":NvimTreeToggle<CR>", { noremap = true })
 
 -- Tabularize
-vim.keymap.set({"n", "v"}, "<Leader>a&", ":Tabularize /&<CR>")
-vim.keymap.set({"n", "v"}, "<Leader>a=", ":Tabularize /^[^=]*\zs=<CR>")
-vim.keymap.set({"n", "v"}, "<Leader>a=>", ":Tabularize /=><CR>")
-vim.keymap.set({"n", "v"}, "<Leader>a:", ":Tabularize /: <CR>")
-vim.keymap.set({"n", "v"}, "<Leader>a::", ":Tabularize /: \zs<CR>")
-vim.keymap.set({"n", "v"}, "<Leader>a,", ":Tabularize /,<CR>")
-vim.keymap.set({"n", "v"}, "<Leader>a,,", ":Tabularize /,\zs<CR>")
-vim.keymap.set({"n", "v"}, "<Leader>a<Bar>", ":Tabularize /<Bar><CR>")
-vim.keymap.set({"n", "v"}, "<Leader>a\\", ":Tabularize /\\<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a&", ":Tabularize /&<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a=", ":Tabularize /^[^=]*\zs=<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a=>", ":Tabularize /=><CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a:", ":Tabularize /: <CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a::", ":Tabularize /: \zs<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a,", ":Tabularize /,<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a,,", ":Tabularize /,\zs<CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a<Bar>", ":Tabularize /<Bar><CR>")
+vim.keymap.set({ "n", "v" }, "<Leader>a\\", ":Tabularize /\\<CR>")
 
 -- GitGutter
 vim.g.gitgutter_override_sign_column_highlight = 0
-vim.g.gitgutter_sign_added='┃'
-vim.g.gitgutter_sign_modified='┃'
-vim.g.gitgutter_sign_removed='◢'
-vim.g.gitgutter_sign_removed_first_line='◥'
-vim.g.gitgutter_sign_modified_removed='◢'
+vim.g.gitgutter_sign_added = '┃'
+vim.g.gitgutter_sign_modified = '┃'
+vim.g.gitgutter_sign_removed = '◢'
+vim.g.gitgutter_sign_removed_first_line = '◥'
+vim.g.gitgutter_sign_modified_removed = '◢'
 
 -- Snippets
 
@@ -275,47 +276,47 @@ vim.g.lastplace_ignore_buftype = "quickfix,nofile,help"
 vim.g.ale_linters_explicit = 1
 vim.g.ale_disable_lsp = 1
 vim.g.ale_linters = {
-  ['c'] = {'clangtidy', 'clangcheck', 'flawfinder'},
-  ['cpp'] = {'clangtidy', 'clangcheck', 'flawfinder'},
-  ['latex'] = {'chktex', 'lacheck', 'proselint', 'vale', 'write-good'},
+    ['c'] = { 'clangtidy', 'clangcheck', 'flawfinder' },
+    ['cpp'] = { 'clangtidy', 'clangcheck', 'flawfinder' },
+    ['latex'] = { 'chktex', 'lacheck', 'proselint', 'vale', 'write-good' },
 }
 
 
 -- Autoformat
-vim.keymap.set({"n", "v"}, "<Leader>kk", ":Autoformat<CR>", {noremap=true})
+vim.keymap.set({ "n", "v" }, "<Leader>kk", ":Autoformat<CR>", { noremap = true })
 
 -- FZF
 -- Customize fzf colors to match your color scheme
 vim.g.fzf_colors = {
-  ['fg']      = {'fg', 'Normal'},
-  ['bg']      = {'bg', 'Normal'},
-  ['hl']      = {'fg', 'Comment'},
-  ['fg+']     = {'fg', 'CursorLine', 'CursorColumn', 'Normal'},
-  ['bg+']     = {'bg', 'CursorLine', 'CursorColumn'},
-  ['hl+']     = {'fg', 'Statement'},
-  ['info' ]   = {'fg', 'PreProc'},
-  ['border']  = {'fg', 'Ignore'},
-  ['prompt']  = {'fg', 'Conditional'},
-  ['pointer'] = {'fg', 'Exception'},
-  ['marker']  = {'fg', 'Keyword'},
-  ['spinner'] = {'fg', 'Label'},
-  ['header']  = {'fg', 'Comment'}
- }
+    ['fg']      = { 'fg', 'Normal' },
+    ['bg']      = { 'bg', 'Normal' },
+    ['hl']      = { 'fg', 'Comment' },
+    ['fg+']     = { 'fg', 'CursorLine', 'CursorColumn', 'Normal' },
+    ['bg+']     = { 'bg', 'CursorLine', 'CursorColumn' },
+    ['hl+']     = { 'fg', 'Statement' },
+    ['info']    = { 'fg', 'PreProc' },
+    ['border']  = { 'fg', 'Ignore' },
+    ['prompt']  = { 'fg', 'Conditional' },
+    ['pointer'] = { 'fg', 'Exception' },
+    ['marker']  = { 'fg', 'Keyword' },
+    ['spinner'] = { 'fg', 'Label' },
+    ['header']  = { 'fg', 'Comment' }
+}
 
 
 
-vim.keymap.set("n", "<leader>b", ":Buffers<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>f", ":Files<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>m", ":Maps<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>c", ":Colors<CR>", {noremap=true})
-vim.keymap.set("n", "<leader>g", ":Rg<CR>", {noremap=true})
+vim.keymap.set("n", "<leader>b", ":Buffers<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>f", ":Files<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>m", ":Maps<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>c", ":Colors<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>g", ":Rg<CR>", { noremap = true })
 
 -- Writing
 vim.g.tex_flavor = "latex"
 vim.cmd("let g:pencil#wrapModeDefault = 'soft'")
 -- vim.g.pencil#wrapModeDefault = 'soft'   --- default is 'hard'
 vim.cmd(
-  [[augroup pencil
+    [[augroup pencil
     autocmd!
     autocmd FileType markdown,mkd      call pencil#init()
     autocmd FileType text,tex,plaintex call pencil#init()
@@ -323,9 +324,9 @@ vim.cmd(
 )
 
 -- UltiSnips
-vim.g.UltiSnipsExpandTrigger="<tab>"
-vim.g.UltiSnipsJumpForwardTrigger="<c-k>"
-vim.g.UltiSnipsJumpBackwardTrigger="<c-j>"
+vim.g.UltiSnipsExpandTrigger = "<tab>"
+vim.g.UltiSnipsJumpForwardTrigger = "<c-k>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<c-j>"
 
 
 require('setup')
@@ -334,10 +335,10 @@ require('lsp')
 
 vim.cmd("colorscheme gruvbox-material")
 
-vim.keymap.set("i", "<Tab>", "pumvisible() ? <C-n> : \\<Tab>", {noremap = true, expr = true})
-vim.keymap.set("i", "<S-Tab>", "pumvisible() ? <C-p> : \\<S-Tab>", {noremap = true, expr = true})
+vim.keymap.set("i", "<Tab>", "pumvisible() ? <C-n> : \\<Tab>", { noremap = true, expr = true })
+vim.keymap.set("i", "<S-Tab>", "pumvisible() ? <C-p> : \\<S-Tab>", { noremap = true, expr = true })
 
-opt.completeopt='menuone,noinsert,noselect'
+opt.completeopt = 'menuone,noinsert,noselect'
 
 -- Make LLVM IR files the correct type
 vim.cmd([[augroup filetype
--- config/.config/nvim/lua/lsp.lua
+++ config/.config/nvim/lua/lsp.lua
@@ -1,4 +1,3 @@
-local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
@@ -21,22 +20,18 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<leader>la', '<cmd>lua require("lspsaga.codeaction").code_action()<CR>', opts)
   buf_set_keymap('v', '<leader>la', ':<C-U>lua require("lspsaga.codeaction").range_code_action()<CR>', opts)
   buf_set_keymap('n', '<leader>ls', '<cmd>lua require("lspsaga.signaturehelp").signature_help()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua require("lspsaga.diagnostic").lsp_jump_diagnostic_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua require("lspsaga.diagnostic").lsp_jump_diagnostic_next()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua require("lspsaga.hover").render_hover_doc()<CR>', opts)
+  buf_set_keymap('n', '[e', '<cmd>lua require("lspsaga.diagnostic"):goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']e', '<cmd>lua require("lspsaga.diagnostic"):goto_next()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua require("lspsaga.hover"):render_hover_doc()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<leader>lo', '<cmd>lua require("lspsaga.outline"):outline()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  end
-
-  if client.resolved_capabilities.document_range_formatting then
-      buf_set_keymap("v", "<leader>lf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-  end
+  buf_set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+  buf_set_keymap("v", "<leader>lf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.document_highlight then
     vim.api.nvim_exec([[
       hi LspReferenceRead cterm=bold ctermbg=darkred guibg=LightYellow
       hi LspReferenceText cterm=bold ctermbg=darkred guibg=LightYellow
@@ -50,57 +45,41 @@ local on_attach = function(client, bufnr)
   end
 end
 
--- require 'snippets'.use_suggested_mappings()
--- require 'snippets'.set_ux(require 'snippets.inserters.vim_input')
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true;
 
--- Use a loop to conveniently both setup defined servers
--- and map buffer local keybindings when the language server attaches
---local servers = {  "rust_analyzer", "vimlsp", "sumeneko" }
---require'lspinstall'.setup()
---local servers = require'lspinstall'.installed_servers()
---for _, lsp in ipairs(servers) do
---nvim_lsp[lsp].setup {
---capabilities = capabilities;
---on_attach = on_attach,
---}
---end
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = { "sumneko_lua", "rust_analyzer", "clangd" }
+}
 
-local function setup_servers()
-  local lsp_installer = require("nvim-lsp-installer")
+local lspconfig = require("lspconfig")
 
-  -- Register a handler that will be called for all installed servers.
-  -- Alternatively, you may also register handlers on specific server instances instead (see example below).
-  lsp_installer.on_server_ready(function(server)
-    local opts = { on_attach = on_attach, capabilities = capabilities }
+lspconfig.clangd.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
 
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-    if server.name == 'sumneko_lua' then
-      opts.settings = { Lua = { diagnostics = { globals = { 'vim' } } } }
-    end
+lspconfig.rust_analyzer.setup {
+    on_attach = on_attach, capabilities = capabilities
+}
 
-    -- This setup() function is exactly the same as lspconfig's setup function.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-  end)
-end
-
-setup_servers()
+lspconfig.sumneko_lua.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = { Lua = { diagnostics = { globals = { 'vim' } } } }
+}
 
 
 --vim.lsp.set_log_level("debug")
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = true,
-  signs = false,
-  update_in_insert = true,
-}
-)
+        vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        signs = false,
+        update_in_insert = true,
+    }
+    )
 
 --
 --
@@ -109,46 +88,46 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 --vim.fn.sign_define("LspDiagnosticsSignError", {text = "ﮊ", numhl = "LspDiagnosticsDefaultError"})
 vim.fn.sign_define("LspDiagnosticsSignError",
-  { text = ">>", numhl = "LspDiagnosticsDefaultSignError", texthl = "LspDiagnosticsDefaultSignError" })
+    { text = ">>", numhl = "LspDiagnosticsDefaultSignError", texthl = "LspDiagnosticsDefaultSignError" })
 vim.fn.sign_define("LspDiagnosticsSignWarning", { text = "", numhl = "LspDiagnosticsDefaultSignWarning" })
 --vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "!!", numhl = "LspDiagnosticsDefaultSignWarning", texthl = "LspDiagnosticsDefaultSignWarning"})
 vim.fn.sign_define("LspDiagnosticsSignInformation",
-  { text = "", numhl = "LspDiagnosticsDefaultSignInformation", texthl = "LspDiagnosticsDefaultSignInformation" })
+    { text = "", numhl = "LspDiagnosticsDefaultSignInformation", texthl = "LspDiagnosticsDefaultSignInformation" })
 vim.fn.sign_define("LspDiagnosticsSignHint",
-  { text = "=>", numhl = "LspDiagnosticsDefaultSignHint", texthl = "LspDiagnosticsDefaultSignHint", gui = bold })
+    { text = "=>", numhl = "LspDiagnosticsDefaultSignHint", texthl = "LspDiagnosticsDefaultSignHint", gui = "bold" })
 
 
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-    "bash",
-    "c",
-    "cmake",
-    "comment",
-    "cpp",
-    "css",
-    "dockerfile",
-    "dot",
-    "go",
-    "help",
-    "html",
-    "javascript",
-    "json",
-    "latex",
-    "llvm",
-    "lua",
-    "make",
-    "markdown",
-    "ninja",
-    "python",
-    "rst",
-    "rust",
-    "toml",
-    "vim",
-    "yaml",
-  }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,
-  },
+    ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "comment",
+        "cpp",
+        "css",
+        "dockerfile",
+        "dot",
+        "go",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "latex",
+        "llvm",
+        "lua",
+        "make",
+        "markdown",
+        "ninja",
+        "python",
+        "rst",
+        "rust",
+        "toml",
+        "vim",
+        "yaml",
+    }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    highlight = {
+        enable = true,
+    },
 }
 
 require("luasnip.loaders.from_vscode").lazy_load()
@@ -157,74 +136,75 @@ local luasnip = require("luasnip")
 local cmp = require 'cmp'
 
 local has_words_before = function()
+  unpack = unpack or table.unpack
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = {
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
+    snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
     },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'omni' },
-  }, {
-    { name = 'buffer' },
-  })
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable( -1) then
+            luasnip.jump( -1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'omni' },
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
+    sources = cmp.config.sources({
+        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
 })
