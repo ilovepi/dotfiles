df1caacd8f60875a614f8b172e4fa989d00ce18c
Paul Kirth <pk1574@gmail.com>
2023-02-15 21:45:09 -0800

    [nvim] Format lua config

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -3,8 +3,8 @@ local fn = vim.fn
 -- Auto install packer.nvim if not exists
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-          install_path })
+    packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+            install_path })
 end
 
 --- options
@@ -23,11 +23,11 @@ vim.g.vimtex_compiler_latexmk = [[ 'options' : [
 
 
 local function init_dirs()
-  local prefix = vim.env.XDG_DATA_HOME or vim.fn.expand("~/.cache")
-  vim.opt.undodir = prefix .. "/nvim/.undo//"
-  vim.opt.backupdir = prefix .. "/nvim/.backup//"
-  vim.opt.directory = prefix .. "/nvim/.swp//"
-  vim.opt.viewdir = prefix .. "/nvim/.views//"
+    local prefix = vim.env.XDG_DATA_HOME or vim.fn.expand("~/.cache")
+    vim.opt.undodir = prefix .. "/nvim/.undo//"
+    vim.opt.backupdir = prefix .. "/nvim/.backup//"
+    vim.opt.directory = prefix .. "/nvim/.swp//"
+    vim.opt.viewdir = prefix .. "/nvim/.views//"
 end
 
 init_dirs()
