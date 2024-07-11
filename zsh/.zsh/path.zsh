916af054fbc7d63a9a781e947f5f73443be88d0f
Paul Kirth <paulkirth@google.com>
2024-07-10 17:20:24 -0700

    [zsh] Remove unused paths from PATH

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,19 +1,12 @@
 # Set parts of path here ...
-# Add Zotero to PATH
-#PATH=$PATH:$HOME/Zotero_linux-x86_64
-
-#add Zotero to path
-#PATH=$PATH:$HOME/bootstrap/Zotero_linux-x86_64
 
 #add local bin dir to PATH
 PATH=$HOME/bin:$PATH
 PATH=$HOME/.local/bin:$PATH
 
 # add Go directories to PATH
-#export GOROOT=/usr/local/go
 export GOPATH=$HOME/workspace/go
 
-#PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 PATH=$GOPATH/bin:$PATH
 
 #add cargo directories to PATH
@@ -41,7 +34,5 @@ localpath="/usr/lib/google-golang/bin:$localpath"
 fi
 
 PATH=${localpath}:$PATH
-
-PATH=${HOME}/arcanist/bin:${PATH}
 
 export PATH
