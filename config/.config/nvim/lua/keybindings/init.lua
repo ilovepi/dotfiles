10fdaec5b7420c0999ad6b3baab0c644c026777c
Paul Kirth <pk1574@gmail.com>
2024-09-02 13:17:57 -0700

    [neovim] Update comments in keybindings

--- config/.config/nvim/lua/keybindings/init.lua
+++ config/.config/nvim/lua/keybindings/init.lua
@@ -1,5 +1,6 @@
 --- Key Bindings
 
+--- Leader Keys
 vim.g.mapleader      = ' '
 vim.g.maplocalleader = '_'
 
@@ -10,24 +11,20 @@ vim.keymap.set("n", "<C-K>", "<C-W>k<C-W>")
 vim.keymap.set("n", "<C-L>", "<C-W>l<C-W>")
 vim.keymap.set("n", "<C-H>", "<C-W>h<C-W>")
 
--- Make splits equal
+--- Make splits equal
 vim.keymap.set("n", "<leader>=", "<C-W>=")
 
--- Bufferline config
+--- Bufferline config
 vim.keymap.set("n", "<S-H>", ":BufferLineCyclePrev<CR>")
 vim.keymap.set("n", "<S-L>", ":BufferLineCycleNext<CR>")
 
-
--- Shortcuts
--- Change Working Directory to that of the current file
-
+--- Change Working Directory to that of the current file
 vim.keymap.set("c", "cwd", "lcd %:p:h")
 vim.keymap.set("c", "cd.", "lcd %:p:h")
 
-
+--- Make j and k work like you expect
 vim.keymap.set("n", "j", "gj", { noremap = true })
 vim.keymap.set("n", "k", "gk", { noremap = true })
-
 
 --- Visual shifting (does not exit Visual mode)
 vim.keymap.set("v", "<", "<gv", { noremap = true })
@@ -47,21 +44,21 @@ vim.keymap.set("v", ".", ":normal .<CR>", { noremap = true })
 --- unfortunately this doesn't work in neovim :(
 --- cmap w!! w !sudo tee % >/dev/null
 
-
--- Some helpers to edit mode
--- http://vimcasts.org/e/14
+--- Some helpers to edit mode
+--- http://vimcasts.org/e/14
 vim.keymap.set("c", "%%", "<C-R>=fnameescape(expand('%:h')).'/'<CR>", { noremap = true })
 vim.keymap.set("v", "ew", ":e %%")
 vim.keymap.set("v", "es", ":sp %%")
 vim.keymap.set("v", "ev", ":vs %%")
 vim.keymap.set("v", "et", ":tabe %%")
 
--- Adjust viewports to the same size
-vim.keymap.set("v", "<Leader>=", "<C-w>=")
+--- Adjust viewports to the same size
+vim.keymap.set("v", "<leader>=", "<C-w>=")
 
--- sane remap of <Esc>
+--- sane remap of <Esc>
 -- vim.keymap.set("v", "jk", "<Esc>=")
 
+--- Disable fzf in terminal
 vim.cmd([[au FileType fzf tunmap <Esc>
           au TermOpen * tnoremap <Esc> <C-\><c-n>
           tnoremap <expr> <C-R> '<C-\><C-N>"'.nr2char(getchar()).'pi'
@@ -74,12 +71,7 @@ vim.cmd([[au FileType fzf tunmap <Esc>
           inoremap <C-k> <C-\><C-N><C-w>k
           inoremap <C-l> <C-\><C-N><C-w>l]])
 
--- map <C-J> <C-W>j
--- map <C-K> <C-W>k
--- map <C-L> <C-W>l
--- map <C-H> <C-W>h
-
--- Stupid shift key fixes
+--- Stupid shift key fixes
 vim.cmd([[command! -bang -nargs=* -complete=file E e<bang> <args>
           command! -bang -nargs=* -complete=file W w<bang> <args>
           command! -bang -nargs=* -complete=file Wq wq<bang> <args>
@@ -92,9 +84,9 @@ vim.cmd([[command! -bang -nargs=* -complete=file E e<bang> <args>
 
 vim.keymap.set("c", "Tabe", "tabe")
 
--- Yank from the cursor to the end of the line, to be consistent with C and D.
+--- Yank from the cursor to the end of the line, to be consistent with C and D.
 vim.keymap.set("n", "Y", "y$")
 
-
+--- Use menus w/ Tab and Shift-Tab
 vim.keymap.set("i", "<Tab>", "pumvisible() ? <C-n> : \\<Tab>", { noremap = true, expr = true })
 vim.keymap.set("i", "<S-Tab>", "pumvisible() ? <C-p> : \\<S-Tab>", { noremap = true, expr = true })
