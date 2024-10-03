222a0a4b3869d0dd79cbd3469374c659978ac7fd
Paul Kirth <paulkirth@google.com>
2024-10-03 09:04:30 -0700

    [neovim] Support MLIR

--- config/.config/nvim/lua/before/llvm.lua
+++ config/.config/nvim/lua/before/llvm.lua
@@ -2,3 +2,8 @@
 vim.cmd([[augroup filetype
           au! BufRead,BufNewFile *.ll set filetype=llvm
           augroup END]])
+-- Make MLIR files the correct type
+vim.cmd([[augroup filetype
+          au! BufRead,BufNewFile *.mlir set filetype=mlir
+          augroup END]])
+
