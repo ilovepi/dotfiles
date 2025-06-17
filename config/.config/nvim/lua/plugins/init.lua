b95913de70b8eaa84e2fb8e24baf4e269a6e2f67
Paul Kirth <paulkirth@google.com>
2025-06-16 19:29:17 -0700

    [nvim] configure lualine with block-style theme
    Configures lualine.nvim with a modern, block-style theme that
    integrates with the existing gruvbox-material colorscheme.

--- config/.config/nvim/lua/plugins/init.lua
+++ config/.config/nvim/lua/plugins/init.lua
@@ -238,8 +238,8 @@ require("lazy").setup({
             require('lualine').setup({
                 options = {
                     theme = 'gruvbox-material',
-                    section_separators = { left = '', right = '' },
-                    component_separators = { left = '', right = '' },
+                    section_separators = { left = '', right = '' },
+                    component_separators = { left = ' ', right = ' ' },
                     disabled_filetypes = { 'packer', 'NvimTree' },
                     always_divide_middle = true,
                 },
