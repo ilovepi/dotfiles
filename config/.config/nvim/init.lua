339741a0edba27d55e31892d5548be1d9eb62c53
Paul Kirth <paulkirth@google.com>
2025-06-16 18:48:13 -0700

    [nvim] Remove pckr remnants
    Removes the pckr directory and updates comments in the main init.lua
    to reflect the switch to lazy.nvim.

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,7 +1,7 @@
 require("options")     --- neovim options
 require("before")      --- configs that run before plugins
 require("keybindings") --- keybindings
-require('plugins')     --- load plugins w/ pckr
+require('plugins')     --- load plugins w/ lazy.nvim
 require('lsp')         --- setup lsp & snippets
 require('dapconfig')   --- debugger config
 require("after")       --- configs that run after plugins
