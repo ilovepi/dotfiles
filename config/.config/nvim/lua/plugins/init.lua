ed9d62a19690671f7ea8f460794f30febd2b8101
Paul Kirth <pk1574@gmail.com>
2026-05-30 13:42:52 -0700

    [nvim] Use vim.uv over deprecated vim.loop

--- config/.config/nvim/lua/plugins/init.lua
+++ config/.config/nvim/lua/plugins/init.lua
@@ -1,6 +1,6 @@
 -- Bootstrap Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
